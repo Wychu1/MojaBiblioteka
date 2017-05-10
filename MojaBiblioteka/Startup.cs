f1782eca9b6afaf5a41c62706cffe2be87a984b9
wychu1 <wychu69@gmail.com>
2017-05-10 11:43:28 +0200

    NowyProjekt
    Start2

---
+++ MojaBiblioteka/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MojaBiblioteka.Startup))]
+namespace MojaBiblioteka
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
